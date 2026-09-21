5e3fae88a1aa063763c1cfde4ef9372a73641e1b
Hamza Saeed <itshamza449@gmail.com>
2026-09-21 17:39:31 +0500

    Update checkoutHeader.js

--- js/checkout/checkoutHeader.js
+++ js/checkout/checkoutHeader.js
@@ -1,39 +1,78 @@
 import { cart } from '../../data/cart.js';
 
+
+
 export function getCheckoutHeaderHTML() {
+
   let cartQuantity = 0;
 
+
+
   cart.forEach((item) => {
+
     cartQuantity += item.quantity;
+
   });
+
+
 
   const cartQuantityText = cartQuantity > 0 ? `${cartQuantity} items` : '';
 
+
+
   return `
+
     <div class="header-content">
+
       <div class="checkout-header-left-section">
-        <a href="amazon.html">
+
+        <a href="index.html">
+
           <img class="amazon-logo" src="images/amazon-logo.png">
+
           <img class="amazon-mobile-logo" src="images/amazon-mobile-logo.png">
+
         </a>
+
       </div>
+
+
 
       <div class="checkout-header-middle-section">
+
         Checkout (<a class="return-to-home-link checkout-count-js-2"
-          href="amazon.html">${cartQuantityText}</a>)
+
+          href="index.html">${cartQuantityText}</a>)
+
       </div>
+
+
 
       <div class="checkout-header-right-section">
+
         <img src="images/icons/checkout-lock-icon.png">
+
       </div>
+
     </div>
+
   `;
+
 }
+
+
 
 export function renderCheckoutHeader() {
+
   const checkoutHeaderElement = document.querySelector('.js-checkout-header');
 
+
+
   if (checkoutHeaderElement) {
+
     checkoutHeaderElement.innerHTML = getCheckoutHeaderHTML();
+
   }
+
 }
+
