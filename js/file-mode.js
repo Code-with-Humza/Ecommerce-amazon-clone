5c03e9a3cf7ced740dabf817fa696c1cf2c01651
Hamza Saeed <itshamza449@gmail.com>
2026-09-21 17:40:26 +0500

    Update file-mode.js

--- js/file-mode.js
+++ js/file-mode.js
@@ -145,8 +145,8 @@ function renderCheckoutHeader() {
   const quantity = cart.reduce((total, item) => total + item.quantity, 0);
   element.innerHTML = `
     <div class="header-content">
-      <div class="checkout-header-left-section"><a href="amazon.html"><img class="amazon-logo" src="images/amazon-logo.png"><img class="amazon-mobile-logo" src="images/amazon-mobile-logo.png"></a></div>
-      <div class="checkout-header-middle-section">Checkout (<a class="return-to-home-link checkout-count-js-2" href="amazon.html">${quantity} items</a>)</div>
+      <div class="checkout-header-left-section"><a href="index.html"><img class="amazon-logo" src="images/amazon-logo.png"><img class="amazon-mobile-logo" src="images/amazon-mobile-logo.png"></a></div>
+      <div class="checkout-header-middle-section">Checkout (<a class="return-to-home-link checkout-count-js-2" href="index.html">${quantity} items</a>)</div>
       <div class="checkout-header-right-section"><img src="images/icons/checkout-lock-icon.png"></div>
     </div>`;
 }
